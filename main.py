7755241735050c8380fd23d524edaad354417d2a
anisaai7571-droid <anisaai7571@gmail.com>
2026-01-16 05:24:11 -0800

    Create main.py
    Add main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+print("Halo 👋")
+print("Saya Anisa, AI sederhana 🤍")
+
+while True:
+    user = input("Kamu: ")
+    if user.lower() in ["keluar", "exit", "bye"]:
+        print("Anisa: Sampai jumpa 😊")
+        break
+    else:
+        print("Anisa: Aku masih belajar, tapi aku dengar kamu 🤍")
